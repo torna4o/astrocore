f2cece439d489a67b6cf20da5638f32e9a47ffb6
Güray Hatipoğlu <62484975+torna4o@users.noreply.github.com>
2026-05-12 13:59:00 +0300

    Update remote.py

--- astrocore/io/remote.py
+++ astrocore/io/remote.py
@@ -1,1 +1,22 @@
+from .base import DataSource
+import requests
 
+
+class RemoteJSON(DataSource):
+
+    def __init__(self, url):
+        self.url = url
+
+    def load(self):
+
+        response = requests.get(self.url)
+        data = response.json()
+
+        return {
+            "t": data["t"],
+            "y": data["y"],
+            "meta": {
+                "source": "remote_json",
+                "url": self.url
+            }
+        }
